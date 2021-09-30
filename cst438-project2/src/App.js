d2b86c8f3e743071fed80d8b731b6be35dc4ef61
rjmorla <41564261+rjmorla@users.noreply.github.com>
2021-09-29 18:30:15 -0700

    added login page -needs backend support

---
+++ cst438-project2/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import Login from './Login';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Preferences from './Preferences';
+
+function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
