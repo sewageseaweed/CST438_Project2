90de3664d40efe4ad4aad44385c5e2283046b695
anggutierrez <gutierrezangel240@gmail.com>
2021-10-14 00:42:27 -0700

    UserController made

---
+++ cst438-project2/src/main/java/com/groupO/project2/UserController.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RestControllerAdvice
+@RequestMapping(path = "/api/users")
+public class UserController {
+
+    private final UserController userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(@RequestParam Optional<String> query) {
+        return userService.getUsers(query);
+    }
+}
