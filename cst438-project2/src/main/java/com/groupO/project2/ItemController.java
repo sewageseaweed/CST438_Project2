d5030a8454270ccf2826147f9a3c45042b4a663f
anggutierrez <gutierrezangel240@gmail.com>
2021-10-20 13:59:57 -0700

    Create, Update, and Delete Items

--- cst438-project2/src/main/java/com/groupO/project2/ItemController.java
+++ cst438-project2/src/main/java/com/groupO/project2/ItemController.java
@@ -4,7 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +29,25 @@ public class ItemController {
     private ItemService itemService;
 
     @GetMapping(value = "/allItems")
-    public List<Item> getUsers() {
+    public List<Item> getItems() {
         return ObjectMapperUtils.mapAll(itemService.getItems(), Item.class);
     }
+
+    @GetMapping(value = "/itemLink/{itemLink}")
+    public Item getItemsByItemLink(@PathVariable("itemLink") String itemLink) {
+        return ObjectMapperUtils.map(itemService.findByItemLink(itemLink), Item.class);
+    }
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<?> saveOrUpdateItem(@RequestBody Item item) {
+        itemService.saveOrUpdateItem(ObjectMapperUtils.map(item, Item.class));
+        return new ResponseEntity("Item added successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{itemLink}")
+    public ResponseEntity<?> deleteItemByItemLink(@PathVariable String itemLink) {
+        itemService.deleteItem(itemService.findByItemLink(itemLink).getId());
+        return new ResponseEntity("Item deleted successfully", HttpStatus.OK);
+    }
+
 }
